6967af72faca09bba97dfc22eeab5181bc9e97cc
NadyaKireyeva <styyypik@gmail.com>
2023-06-11 17:42:58 +0600

    Added checkbox functionality. Fixced layout, class naming

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,27 @@
+import { useState } from 'react';
+
 import './Card.css';
+
 function Card(props) {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const checkboxChangeHandler = () => {
+    setIsChecked(!isChecked);
+  };
+
+  const cardClassName = isChecked ? 'card checked' : 'card';
+
   return (
-    <div className="card">
-      <h2>{props.title}</h2>
-      <img src={props.img} alt="cat" />
+    <div className={cardClassName}>
+      <div className="card__block">
+        <h2 className="card__block-title">{props.title}</h2>
+        <input
+          className="card__block-checkbox"
+          type="checkbox"
+          checked={isChecked}
+          onChange={checkboxChangeHandler}></input>
+      </div>
+      <img className="card__img" src={props.img} alt="cat" />
       <p className="card__content-text">{props.descr}</p>
     </div>
   );
