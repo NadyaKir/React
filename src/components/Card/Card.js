42c100adaf1f823d0e49996ff46e262244351bc2
NadyaKireyeva <styyypik@gmail.com>
2023-06-12 14:24:25 +0600

    Merge task-1 into task-2

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -13,16 +13,16 @@ function Card(props) {
 
   return (
     <div className={cardClassName}>
-      <div className="card__block">
-        <h2 className="card__block-title">{props.title}</h2>
+      <div className="card-block">
+        <h2 className="card-block__title">{props.title}</h2>
         <input
-          className="card__block-checkbox"
+          className="card-block-checkbox"
           type="checkbox"
           checked={isChecked}
           onChange={checkboxChangeHandler}></input>
       </div>
       <img className="card__img" src={props.img} alt="cat" />
-      <p className="card__content-text">{props.descr}</p>
+      <p className="card__text">{props.descr}</p>
     </div>
   );
 }
