65362294067579652ac5e3bb447e43aa6a8d7794
NadyaKir <styyypik@gmail.com>
2023-08-05 21:04:26 +0600

    Modal closing when CardWrapper click: fixed

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -4,9 +4,18 @@ import CardWrapper from '../UI/CardWrapper';
 import { ModalWrapper } from './Modal.styled';
 
 const Modal = ({ children, closeModal }) => {
+  const handleWrapperClick = (e) => {
+    // Проверяем, был ли клик на самом модальном окне (ModalWrapper)
+    // Если да, тогда закрывать модальное окно, если нет - не закрывать
+    if (e.target === e.currentTarget) {
+      console.log(e.target, e.currentTarget)
+      closeModal();
+    }
+  }
+
   return (
-    <ModalWrapper onClick={closeModal}>
-      <CardWrapper>{children}</CardWrapper>
+    <ModalWrapper onClick={handleWrapperClick}>
+      <CardWrapper >{children}</CardWrapper>
     </ModalWrapper>
   );
 };
