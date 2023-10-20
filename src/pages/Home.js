f7db52a8aa2671f97807d9a7a4b8f4e15dfc9eb8
NadyaKir <styyypik@gmail.com>
2023-10-21 02:20:10 +0600

    fix: checkbox ViewOnly stay true after return from card page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,15 @@
 import Content from '../components/Content/Content';
 
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
 const HomePage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type: 'SET_READ_ONLY' });
+  }, [dispatch]);
+
   return <Content />;
 };
 
